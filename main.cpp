8c2d459244519c9bd3d9767c9d9e7738d8138c79
Igorsb3000 <igorsb3000@gmail.com>
2019-03-04 11:45:38 -0300

    arquivo certo

--- main.cpp
+++ main.cpp
@@ -1,44 +1,90 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+#define PI 3.14
 using namespace std;
 
+void func_planas(int indice, int param1, int param2){ //, int *area_result, int *perimetro_result
+	
+	double area, altura, base, perimetro, raio;
 
-int MDC(int p, int q){
-	int vetor[5]={2, 3, 5, 7, 9};
-	int mdc_res = 1;
-	if(q == 0)
-	{
-		return p;
-
-	}
-	else if(p == 0)
-	{	
-		return q;
-
-	}
-	else
-	{	
-		for(int i=0; i<5; i++){
-			if((p % vetor[i] == 0) and (q % vetor[i] == 0)){
-				mdc_res = mdc_res * vetor[i];
-			}
-
-		}
+	if(indice == 0){
+		base = param1;
+		altura = param2;
+		area = base * altura;
+		perimetro = 2 * (base + altura);
+		cout << "Area do retangulo: " << area << endl;
+		cout << "Perimetro do retangulo: " << perimetro << endl;
 		
 
 	}
-	return mdc_res;
+	else if(indice == 1){
+		raio = param1;
+		area = PI * raio;
+		perimetro = (2 * PI * raio);
+		cout << "Area do circulo: " << area << endl;
+		cout << "Perimetro do circulo: " << perimetro << endl;
+		
+	}
+	else if(indice == 2){
+		base = param1;
+		altura = param2;
+		area = (base * altura)/2;
+		perimetro = base + base + base;
+		cout << "Area do triangulo: " << area << endl;
+		cout << "Perimetro do triangulo: " << perimetro << endl;
+		
+	}
+	else if(indice == 3){
+		base = param1;
+		area = base * base;
+		perimetro = 4 * base;
+		cout << "Area do quadrado: " << area << endl;
+		cout << "Perimetro do quadrado: " << perimetro << endl;
+
+	}
+	//*area_result = area;
+	//*perimetro_result = perimetro;
+	
+	
 }
-int main(){
-	int p, q, resultado;
 
-	cout << "Digite um inteiro " << endl;
-	cin >> p;
-	cout << "Digite um inteiro " << endl;
-	cin >> q;
+void func_espaciais(int i){
+	
 
-	resultado = MDC(p, q);
+}
 
-	cout << "O MDC de " << p << " e "<< q << " eh: "<< resultado << endl;
+int main(int argc, char *argv[]){ 
+	std::string planas[] = {"retangulo","circulo","triangulo","quadrado"};
+	std::string espaciais[] = {"piramide", "cubo", "paralelepipedo", "esfera"};
+	std::string tipo_figura = argv[1];
+	//cout << argc << endl;
+	int *area = new int; 
+	int *perimetro = new int;
 
+	//Poderia utilizar um vetor para receber a quantidade exata de argumentos, tava dando falha de segmentação
+	int *param = new int[argc-2];
+	int j;
+	for(j=0; j<(argc-2); j++){
+		param[j] = atoi(argv[j+2]);
+	} 
+
+
+	for (int i = 0; i < 4; i++){
+		if (!tipo_figura.compare(planas[i])) {
+			func_planas(i, param[0], param[1]); //, area, perimetro
+
+			//cout << "Area: "<< *area << endl;
+			//cout << "Perimetro: " << *perimetro << endl;
+			
+		}
+		if (!tipo_figura.compare(espaciais[i])){
+			func_planas(i, param[0], param[1], param[2]);
+		}
+	}
+
+	delete area;
+	delete perimetro;
+	delete[] param;
 	return 0;
 }
