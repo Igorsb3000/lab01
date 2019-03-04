a290fbaa268fae22173547a8c8ff828d59b275af
Igorsb3000 <igorsb3000@gmail.com>
2019-03-04 20:35:14 -0300

    main

---
+++ lab01_Q1.cpp
@@ -0,0 +1,36 @@
+#include "lab01_Q1_cabecalho.hpp"
+
+int main(int argc, char *argv[]){ 
+	std::string planas[] = {"retangulo","circulo","triangulo","quadrado"};
+	std::string espaciais[] = {"piramide", "cubo", "paralelepipedo", "esfera"};
+	if(argv[1] == NULL){
+		cout << "ERRO: parâmetro inválido!" << endl << endl;
+		cout << "As opções de parâmetro são:" << endl << endl;
+		cout << "* Figuras Planas: triangulo, retangulo, quadrado ou circulo junto das suas medidas." << endl << endl;
+		cout << "* Figuras Espaciais: piramide, cubo, paralelepipedo ou esfera junto das suas medidas." << endl << endl;
+		cout << "Ex: ./exe retangulo 5 3" << endl;
+		return 0;
+	}
+	std::string tipo_figura = argv[1];
+
+	int *param = new int[argc-2];
+	int j;
+	for(j=0; j<(argc-2); j++){
+		param[j] = atoi(argv[j+2]);
+	} 
+
+	for (int i = 0; i < 4; i++){
+		if (!tipo_figura.compare(planas[i])) {
+			func_planas(i, param[0], param[1]);
+			break;
+			
+		}
+		if (!tipo_figura.compare(espaciais[i])){
+			func_espaciais(i, param[0], param[1], param[2]);
+			break;
+		}
+	}
+
+	delete[] param;
+	return 0;
+}
