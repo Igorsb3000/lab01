d71f92dbda837affdd2a240932075659eac1ef27
Igorsb3000 <igorsb3000@gmail.com>
2019-03-04 20:36:02 -0300

    cabecalho

---
+++ lab01_Q1_cabecalho.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#define PI 3.1415
+using namespace std;
+
+void func_planas(int indice, int param1, int param2);
+
+void func_espaciais(int indice, int param1, int param2, int param3);
+
