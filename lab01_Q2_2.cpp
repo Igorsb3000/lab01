797d92330ec0bc3177f48f197ff461264c1cfc9a
HiltonThallyson <hilton.thallyson@hotmail.com>
2019-03-06 20:24:06 -0300

    Alteracoes na tarefa 1 e 2

--- lab01_Q2_2.cpp
+++ lab01_Q2_2.cpp
@@ -18,25 +18,27 @@ int fatorial(int n){
 }
 
 void func_seno(double radiano){
-	double seno;
+	double seno = 0;
+	double erro = 1;
+	int k=0, N=0;
+	do{
+		seno = seno + (pow(-1,k)*pow(radiano,1+2*k))/fatorial(1+2*k);
+		cout << "seno: " << seno << endl;
+		erro = seno - sin(radiano);
+		if(erro < 0){
+			erro = erro*(-1);
+		}
+		cout << erro << endl;
+		N++;
+		k++;
+	}while (erro > 0.0001 );
 
-	seno = radiano - pow(radiano,3)/fatorial(3) + pow(radiano,5)/fatorial(5);
 	cout << "Seno de " << radiano << " : " << seno << endl;
-
+	cout << "erro: " << erro << "." << endl;
+	cout << "N: " << N << "." << endl;
 	//Usando a função da biblioteca
 	seno = sin(radiano);
 	cout << "Seno da biblioteca de " << radiano << " : " << seno << endl << endl;
-
-}
-
-void func_cosseno(double radiano){
-	double cosseno;
-	cosseno = 1 - pow(radiano,2)/fatorial(2) + pow(radiano,4)/fatorial(4);
-	cout << "Coseno de " << radiano << " : " << cosseno << endl;
-
-	//Usando a função da biblioteca
-	cosseno = cos(radiano);
-	cout << "Cosseno da biblioteca de " << radiano << " : " << cosseno << endl;
 
 }
 
@@ -47,7 +49,6 @@ int main(){
 	cin >> valor_radiano;
 
 	func_seno(valor_radiano);
-	func_cosseno(valor_radiano);
 
 	return 0;
 }
