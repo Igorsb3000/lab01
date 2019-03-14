4c5600f0c93e7ef0ed43e8d2c9a24af3b4ca0c68
Igorsb3000 <igorsb3000@gmail.com>
2019-03-14 10:48:35 -0300

    Ultima versão

--- Q2_1_corpo.cpp
+++ Q2_1_corpo.cpp
@@ -1,10 +1,6 @@
-#include <iostream>
-#include <cmath>
+#include "Q2_1_cabecalho.h"
 
-#define PI 3.14159265
-using namespace std;
-
-int fatorial(int n){
+double fatorial(int n){
 	if(n == 0)
 	{
 		return 1;
@@ -19,35 +15,24 @@ int fatorial(int n){
 
 void func_seno(double radiano){
 	double seno;
-
+	//Usando séries de Taylor
 	seno = radiano - pow(radiano,3)/fatorial(3) + pow(radiano,5)/fatorial(5);
 	cout << "Seno de " << radiano << " : " << seno << endl;
 
 	//Usando a função da biblioteca
 	seno = sin(radiano);
-	cout << "Seno da biblioteca de " << radiano << " : " << seno << endl << endl;
+	cout << "Seno da cmath " << radiano << " : " << seno << endl << endl;
 
 }
 
 void func_cosseno(double radiano){
 	double cosseno;
+	//Usando séries de Taylor
 	cosseno = 1 - pow(radiano,2)/fatorial(2) + pow(radiano,4)/fatorial(4);
 	cout << "Coseno de " << radiano << " : " << cosseno << endl;
 
 	//Usando a função da biblioteca
 	cosseno = cos(radiano);
-	cout << "Cosseno da biblioteca de " << radiano << " : " << cosseno << endl;
+	cout << "Cosseno da cmath " << radiano << " : " << cosseno << endl;
 
-}
-
-
-int main(){
-	double valor_radiano;
-	cout << "Digite um valor (em radianos) de um angulo x, -180 rad <= x <= 180 rad: " << endl;
-	cin >> valor_radiano;
-
-	func_seno(valor_radiano);
-	func_cosseno(valor_radiano);
-
-	return 0;
 }
