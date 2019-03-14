521ec9ea8eef38e7dd33893bff13d5ab5085d445
Igorsb3000 <igorsb3000@gmail.com>
2019-03-14 11:08:46 -0300

    Ultima versão

---
+++ Q2_1_cabecalho.h
@@ -0,0 +1,14 @@
+#ifndef Q2_1_H
+#define Q2_1_H
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double fatorial(int n);
+
+void func_seno(double radiano);
+
+void func_cosseno(double radiano);
+
+#endif
