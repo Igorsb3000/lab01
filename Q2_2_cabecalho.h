4927580ef24dc2811dcc43c0f767036a20b15a5f
Igorsb3000 <igorsb3000@gmail.com>
2019-03-14 09:56:47 -0300

    Ultima versão

--- Q2_2_cabecalho.h
+++ Q2_2_cabecalho.h
@@ -1,3 +1,6 @@
+#ifndef Q2_2_H
+#define	Q2_2_H
+
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -5,3 +8,5 @@ using namespace std;
 double fatorial(int n);
 
 void func_seno(double radiano);
+
+#endif
